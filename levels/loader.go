7ac19316fc1a08ee9966822724165f1b82d4f0a1
Asher Buk <2asherbuk@gmail.com>
2026-05-22 16:28:57 +0300

    levels: upload onboarding protocol

---
+++ levels/loader.go
@@ -0,0 +1,47 @@
+// Copyright 2026 Asher Buk
+// SPDX-License-Identifier: Apache-2.0
+// https://github.com/AshBuk/goscii
+
+package levels
+
+import (
+	"embed"
+	"fmt"
+	"path"
+
+	"gopkg.in/yaml.v3"
+
+	"github.com/AshBuk/goscii/engine"
+)
+
+//go:embed onboarding
+var FS embed.FS
+
+type Level struct {
+	ID      string            `yaml:"id"`
+	Title   string            `yaml:"title"`
+	Concept string            `yaml:"concept"`
+	Story   string            `yaml:"story"`
+	Hints   []string          `yaml:"hints"`
+	Answer  string            `yaml:"answer"`
+	Check   engine.CheckRule   `yaml:"check"`
+	Capture map[string]string `yaml:"capture"`
+}
+
+func Load(levelPath string) (*Level, string, error) {
+	yamlData, err := FS.ReadFile(path.Join(levelPath, "level.yaml"))
+	if err != nil {
+		return nil, "", fmt.Errorf("read level.yaml: %w", err)
+	}
+	var l Level
+	if err := yaml.Unmarshal(yamlData, &l); err != nil {
+		return nil, "", fmt.Errorf("parse level.yaml: %w", err)
+	}
+
+	tmplData, err := FS.ReadFile(path.Join(levelPath, "template.txt"))
+	if err != nil {
+		return nil, "", fmt.Errorf("read template.txt: %w", err)
+	}
+
+	return &l, string(tmplData), nil
+}
