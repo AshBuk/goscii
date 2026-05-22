2a4835ace09534ce733228d29bc1d05012d6c365
Asher Buk <2asherbuk@gmail.com>
2026-05-22 16:25:54 +0300

    feat(assets): embed astronaut sprite pack

---
+++ assets/assets.go
@@ -0,0 +1,10 @@
+// Copyright 2026 Asher Buk
+// SPDX-License-Identifier: Apache-2.0
+// https://github.com/AshBuk/goscii
+
+package assets
+
+import "embed"
+
+//go:embed astronaut
+var FS embed.FS
